0dc17c233582c7e196f6f7dab8af3be92835ce25
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2022-06-16 12:17:44 -0400

    Adds User Authentications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,13 +16,18 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      render json: @user, status: :created, location: @user
+    @token = encode({id: @user.id})
+    render json: {
+      user: @user.attributes.except("password_digest"),
+      token: @token
+      }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+    render json: @user.errors, status: :unprocessable_entity
     end
   end
+  
 
   # PATCH/PUT /users/1
   def update
@@ -46,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
