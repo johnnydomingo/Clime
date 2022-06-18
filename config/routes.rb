4462cfe592ff43444ae4b57914292361e51af6c7
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2022-06-18 18:05:47 -0400

    Comments out user authentication as this will be PMVP

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
   resources :cities
-  resources :users, only: [:index, :show, :create, :update]
+  # resources :users, only: [:index, :show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
