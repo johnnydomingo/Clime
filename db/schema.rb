184568e8085fad34f8888ceb224ca52462554a08
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2022-06-21 14:52:26 -0400

    Cleans up code for back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_193857) do
+ActiveRecord::Schema.define(version: 2022_06_21_184601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,20 +19,8 @@ ActiveRecord::Schema.define(version: 2022_06_16_193857) do
     t.string "location"
     t.integer "temperature"
     t.text "image"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_cities_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "cities", "users"
 end
